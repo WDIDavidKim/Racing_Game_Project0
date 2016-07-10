bc147c12b9dbe9a62aed98fd8dd8c0015ef1abde
WDIDavidKim <davidjkim1984@gmail.com>
2016-07-10 12:02:21 -0700

    Added .animate with prevent default from constantly moving across.  Not sure if this is correct.

--- js.js
+++ js.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
 
-  $(".container").width(window);
+  $("document").width(window);
 
   $(document).keyup(function(e) {
     if(e.which == 48) { //button 0
-      $(".player1").css({marginLeft: "+=3%"});
+      event.preventDefault(e);
+      $(".player1").animate({marginLeft: "+=3%"});
       }
     });
 
   $(document).keyup(function(e) {
     if(e.which == 49) { //button 1
-      $(".player2").css({marginLeft: "+=3%"});
+      event.preventDefault(e);
+      $(".player2").animate({marginLeft: "+=3%"});
       }
     });
 
