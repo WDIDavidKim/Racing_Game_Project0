144a37284e316df1a8e174893430177d5d4fbd95
WDIDavidKim <davidjkim1984@gmail.com>
2016-07-10 13:49:18 -0700

    Added some more styling.  Still trying to figure out max width as a stopping point for jquery.

--- js.js
+++ js.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
 
-  $("document").width(window);
+  // $("document").width(window);
 
   $(document).keyup(function(e) {
     if(e.which == 48) { //button 0
       event.preventDefault(e);
-      $(".player1").animate({marginLeft: "+=3%"});
+      $(".player1").animate({marginLeft: "+=25px"});
+      // if($(".player1").finish({marginRigh: "600px"});
+    // alert("Player 1 Wins");
       }
     });
 
   $(document).keyup(function(e) {
     if(e.which == 49) { //button 1
       event.preventDefault(e);
-      $(".player2").animate({marginLeft: "+=3%"});
+      $(".player2").animate({marginLeft: "+=25px"});
       }
     });
 
