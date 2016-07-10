c3ada8f03cb1c19c2649f46c555ffebad726ec19
WDIDavidKim <davidjkim1984@gmail.com>
2016-07-10 10:01:19 -0700

    attempt at js with width() jquery but no go

--- js.js
+++ js.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
+
+  $(".container").width(window);
+
   $(document).keyup(function(e) {
     if(e.which == 48) { //button 0
       $(".player1").css({marginLeft: "+=3%"});
-     }
-   });
+      }
+    });
+
   $(document).keyup(function(e) {
     if(e.which == 49) { //button 1
       $(".player2").css({marginLeft: "+=3%"});
-    }
-  });
+      }
+    });
 
 
 
