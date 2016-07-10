3d7e80aebacd540f051b365dc2a14b11d27fffee
WDIDavidKim <davidjkim1984@gmail.com>
2016-07-09 23:45:56 -0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $(document).keyup(function(e) {
+    if(e.which == 48) { //button 0
+      $(".player1").css({marginLeft: "+=3%"});
+     }
+   });
+  $(document).keyup(function(e) {
+    if(e.which == 49) { //button 1
+      $(".player2").css({marginLeft: "+=3%"});
+    }
+  });
+
+
+
+
+
+
+
+});
