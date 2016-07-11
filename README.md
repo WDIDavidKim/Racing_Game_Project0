e638280b41a2810851aeaec6d687cab1b3668cc3
WDIDavidKim <davidjkim1984@gmail.com>
2016-07-11 14:37:13 -0700

    Updated indentation and minimal styling

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Racing_Game_Project0
 
-SORRY HAD TO USE ALERT FOR NOW. Will work on a hidden display after.
-
 Player 1 is 0
 Player 2 is 1
 
@@ -10,6 +8,7 @@ JQuery, index.html, js.js, sytle.css, ASCII keys
 
 Still has some bugs that need to be sorted out including the alert.
 After the player has been alerted a winner the piece will still move on with another alert.
+Alert has been fixed!
 
 #Features
 -yes
--- js.js
+++ js.js
@@ -1,48 +1,48 @@
 $(document).ready(function() {
 
-player1();
-player2();
+  player1();
+  player2();
 
-function player1(){
-  $(document).keyup(function(e) {
-    if(e.which == 49) { //button 1
-      event.preventDefault(e);
-      $(".player1").css("left", ("+=40px"));
+  function player1(){
+    $(document).keyup(function(e) {
+      if(e.which == 49) { //button 1
+        event.preventDefault(e);
+        $(".player1").css("left", ("+=40px"));
         if ($(".container").width() <= $(".player1").offset().left + $(".player1").outerWidth()) {
           stop();
           $(".player1Wins").show();
-      }
+        }
       }
     });
-}
-function player2(){
-  $(document).keyup(function(e) {
-    if(e.which == 48) { //button 0
-      event.preventDefault(e);
-      $(".player2").css("left", ("+=40px"));
+  }
+  function player2(){
+    $(document).keyup(function(e) {
+      if(e.which == 48) { //button 0
+        event.preventDefault(e);
+        $(".player2").css("left", ("+=40px"));
         if ($(".container").width() <= $(".player2").offset().left + $(".player2").outerWidth()) {
           stop();
           $(".player2Wins").show();
-      }
+        }
       }
     });
-}
-function stop() {
-  $('.player1').clearQueue();
-  $('.player1').stop();
-  $('.player2').clearQueue();
-  $('.player2').stop();
-  $(document).off("keyup");
-}
+  }
+  function stop() {
+    $('.player1').clearQueue();
+    $('.player1').stop();
+    $('.player2').clearQueue();
+    $('.player2').stop();
+    $(document).off("keyup");
+  }
 
 
-$("button").click(function(restart){
-  stop();
-  $(".player2Wins").hide();
-  $(".player1Wins").hide();
-  player1();
-  player2();
-  $(".player1").css({'left':'0','opacity':'1'});
-  $(".player2").css({'left':'0','opacity':'1'});
-    });
+  $("button").click(function(restart){
+    stop();
+    $(".player2Wins").hide();//hides div for player2 win
+    $(".player1Wins").hide();//hides div for player1 win
+    player1();
+    player2();
+    $(".player1").css({'left':'0','opacity':'.8'});
+    $(".player2").css({'left':'0','opacity':'.8'});
+  });
 });
