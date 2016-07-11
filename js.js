f141db0a0bf98ee021e558aa78bc0fc71fa005fa
WDIDavidKim <davidjkim1984@gmail.com>
2016-07-10 20:44:37 -0700

    Styling changed still struggling with end of page jquery.

--- js.js
+++ js.js
@@ -1,27 +1,35 @@
 $(document).ready(function() {
 
-  // $("document").width(window);
-
+function player1(){}
   $(document).keyup(function(e) {
     if(e.which == 48) { //button 0
-      event.preventDefault(e);
-      $(".player1").animate({marginLeft: "+=25px"});
-      // if($(".player1").finish({marginRigh: "600px"});
-    // alert("Player 1 Wins");
+      // event.preventDefault(e);
+      $(".player1").css("left", ("+=25px"));
+        if ($(".container").width() <= $(".player1").offset().left + $(".player1").outerWidth()) {
+          stopMvmt();
+      }
       }
     });
 
+
+function player2(){}
   $(document).keyup(function(e) {
     if(e.which == 49) { //button 1
-      event.preventDefault(e);
-      $(".player2").animate({marginLeft: "+=25px"});
+      // event.preventDefault(e);
+      $(".player2").css("left", ("+=25px"));
+        if ($(".container").width() <= $(".player2").offset().left + $(".player2").outerWidth()) {
+          stopMvmt();
       }
+      }
+
     });
-
-
-
-
-
+    function stopMvmt() {
+      $('.player1').clearQueue();
+      $('.player1').stop();
+      $('.player2').clearQueue();
+      $('.player2').stop();
+      $(document).off("keyup");
+      }
 
 
 });
