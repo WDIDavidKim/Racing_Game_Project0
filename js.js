9db26643e3cd5f58b757c805954bc20618244c25
WDIDavidKim <davidjkim1984@gmail.com>
2016-07-10 22:40:21 -0700

    Created a restart button.  Working on another alert style method.

--- js.js
+++ js.js
@@ -3,35 +3,38 @@ $(document).ready(function() {
 function player1(){
 }
   $(document).keyup(function(e) {
-    if(e.which == 49) { //button 0
+    if(e.which == 49) { //button 1
       event.preventDefault(e);
       $(".player1").css("left", ("+=25px"));
         if ($(".container").width() <= $(".player1").offset().left + $(".player1").outerWidth()) {
-          stopMvmt();
+          stop();
+          alert("Player 1 has won!");
       }
       }
     });
-
 
 function player2(){
 }
   $(document).keyup(function(e) {
-    if(e.which == 48) { //button 1
+    if(e.which == 48) { //button 0
       event.preventDefault(e);
       $(".player2").css("left", ("+=25px"));
         if ($(".container").width() <= $(".player2").offset().left + $(".player2").outerWidth()) {
-          stopMvmt();
+          stop();
+          alert("Player 2 has won!");
       }
       }
-
     });
-function stopMvmt() {
+
+function stop() {
   $('.player1').clearQueue();
   $('.player1').stop();
   $('.player2').clearQueue();
   $('.player2').stop();
   $(document).off("keyup");
       }
+
+
 
 
 });
