efb868b9763d0ab42c63f2d191c99950e79a0421
WDIDavidKim <davidjkim1984@gmail.com>
2016-07-11 10:59:19 -0700

    updated restart button to stop from moving forward

--- js.js
+++ js.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
 
-
-
+player1();
+player2();
 
 function player1(){
-}
   $(document).keyup(function(e) {
     if(e.which == 49) { //button 1
       event.preventDefault(e);
@@ -15,9 +14,8 @@ function player1(){
       }
       }
     });
-
-function player2(){
 }
+function player2(){
   $(document).keyup(function(e) {
     if(e.which == 48) { //button 0
       event.preventDefault(e);
@@ -28,20 +26,21 @@ function player2(){
       }
       }
     });
-
+}
 function stop() {
   $('.player1').clearQueue();
   $('.player1').stop();
   $('.player2').clearQueue();
   $('.player2').stop();
-  // $(document).off("keyup");
-      }
+  $(document).off("keyup");
+}
+
 
 $("button").click(function(restart){
+  stop();
+  player1();
+  player2();
   $(".player1").css({'left':'0','opacity':'1'});
   $(".player2").css({'left':'0','opacity':'1'});
     });
-
-
-
 });
